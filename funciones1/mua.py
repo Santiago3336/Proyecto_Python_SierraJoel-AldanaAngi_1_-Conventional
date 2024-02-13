09c548f651721dd0e7e4e36fc920dafcdae98796
angi <angialexandraaldana@gmail.com>
2024-02-13 14:08:56 -0500

    feat :sparkles: Nuevos condicionales

--- funciones1/mua.py
+++ funciones1/mua.py
@@ -17,6 +17,7 @@ if Options == 'Agregar':
         "telefonos": str(input("Telefonos: ")),
         "Documento": str(input("Documento: ")),
         "Estado": str(input("Estado: ")),
+    if lista_campers["Estado"].lower()=="graduado" 
         "Riesgo":  str(input("Riesgo: ")),
     }
     if lista_campers["Estado"].lower() == "inscrito":
@@ -27,6 +28,42 @@ if Options == 'Agregar':
             
     if lista_campers["Estado"].lower() == "aprobado":
         inscritos["campus"]["aprobado"].append(lista_campers)
+        
+        with open("camper.json", 'w') as outfile:
+            json.dump(inscritos, outfile, indent=2)
+
+    if lista_campers["Estado"].lower() == "desaprobado":
+        inscritos["campus"]["desaprobado"].append(lista_campers)
+        
+        with open("camper.json", 'w') as outfile:
+            json.dump(inscritos, outfile, indent=2)
+
+    if lista_campers["Estado"].lower() == "filtrado":
+        inscritos["campus"]["filtrado"].append(lista_campers)
+        
+        with open("camper.json", 'w') as outfile:
+            json.dump(inscritos, outfile, indent=2)
+
+    if lista_campers["Estado"].lower() == "cursando":
+        inscritos["campus"]["cursando"].append(lista_campers)
+        
+        with open("camper.json", 'w') as outfile:
+            json.dump(inscritos, outfile, indent=2)
+
+    if lista_campers["Estado"].lower() == "graduado":
+        inscritos["campus"]["graduado"].append(lista_campers)
+        
+        with open("camper.json", 'w') as outfile:
+            json.dump(inscritos, outfile, indent=2)
+
+    if lista_campers["Estado"].lower() == "expulsado":
+        inscritos["campus"]["expulsado"].append(lista_campers)
+        
+        with open("camper.json", 'w') as outfile:
+            json.dump(inscritos, outfile, indent=2)
+
+    if lista_campers["Estado"].lower() == "retirado":
+        inscritos["campus"]["retirado"].append(lista_campers)
         
         with open("camper.json", 'w') as outfile:
             json.dump(inscritos, outfile, indent=2)
