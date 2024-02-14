af81999a8dc007c9ff5e49701a6a8b877807cdb3
Angi <Angialexandraaldana@gmail.com>
2024-02-13 19:27:38 -0500

    feat :sparkles: Añadir funciones, establecer parte de codigo en una funcion

--- funciones1/mua.py
+++ funciones1/mua.py
@@ -1,24 +1,24 @@
 import json
+def lista_campers():
 
+    with open("camper.json") as file:
+        inscritos = json.load(file)
 
-with open("camper.json") as file:
-    inscritos = json.load(file)
+    Options = input('¿Quieres agregar o actualizar? ')
+    Options = Options.capitalize()
 
-Options = input('¿Quieres agregar o actualizar? ')
-Options = Options.capitalize()
-
-if Options == 'Agregar':
-    lista_campers = {
-        "nombre": str(input("nombre: ")),
-        "apellido1": str(input("apellido: ")),
-        "apellido2": str(input("apellido2: ")),
-        "direccion": str(input("direccion: ")),
-        "acudiente": str(input("Acudiente: ")),
-        "telefonos": str(input("Telefonos: ")),
-        "Documento": str(input("Documento: ")),
-        "Estado": str(input("Estado: ")),
-        "Riesgo":  str(input("Riesgo: ")),
-    }
+    if Options == 'Agregar':
+        lista_campers = {
+            "nombre": str(input("nombre: ")),
+            "apellido1": str(input("apellido: ")),
+            "apellido2": str(input("apellido2: ")),
+            "direccion": str(input("direccion: ")),
+            "acudiente": str(input("Acudiente: ")),
+            "telefonos": str(input("Telefonos: ")),
+            "Documento": str(input("Documento: ")),
+            "Estado": str(input("Estado: ")),
+            "Riesgo":  str(input("Riesgo: ")),
+             }
     if lista_campers["Estado"].lower() == "inscrito":
         inscritos["campus"]["inscritos"].append(lista_campers)
 
--- funciones1/notas.py
+++ funciones1/notas.py
@@ -1,4 +1,10 @@
+import json
 import notas
+from mua import lista_campers
+
+with open("camper.json") as file:
+        inscritos = json.load(file)
+
 
 #Notas de ingreso
 notas=int(input('Ingrese la nota 1'))
@@ -7,4 +13,6 @@ notas2=int(input('Ingrese la nota 2'))
 nota=(notas+notas2)/2
 
 if nota>60:
+    if lista_campers["Estado"].lower() == "aprobado":
+        inscritos["campus"]["aprobado"].append(lista_campers)
     
